509f4a9147b8d3cb5919d3179f79cf520568b5d2
anothersam22 <113322947+anothersam22@users.noreply.github.com>
2022-11-24 16:01:25 -0800

    itertools cycle example

--- game.py
+++ game.py
@@ -4,6 +4,15 @@ Wilson A01266055
 
 """
 
+import itertools
+
+choices = []
+boolean_generator = itertools.cycle([True, False])
+for _ in range(20):
+    choices.append(next(boolean_generator))
+
+print(choices)
+
 
 def main():
     pass
