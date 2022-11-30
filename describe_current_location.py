9cf8bae90ee7a36e5e59d9bf3d0768514b9364af
WilsonSue <113160128+WilsonSue@users.noreply.github.com>
2022-11-30 00:38:33 -0800

    Add more choices to describe_current_location.py

--- describe_current_location.py
+++ describe_current_location.py
@@ -1,3 +1,15 @@
+"""
+Wilson & Sam
+A01266055 A01337600
+"""
+
+
+def describe_current_location():
+    """
+
+    :return:
+    """
+    location = make_character("Dave")
 def describe_current_location(game_board: dict, character: dict) -> str:
     """
     A function to find and describe current location of player
