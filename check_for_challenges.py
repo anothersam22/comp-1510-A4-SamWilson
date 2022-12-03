dc192b1ec08b40480bfb6298155c5b092f165f89
WilsonSue <113160128+WilsonSue@users.noreply.github.com>
2022-12-03 01:48:52 -0800

    Add docstring

--- check_for_challenges.py
+++ check_for_challenges.py
@@ -1,6 +1,13 @@
 
 
-def check_for_challenge(board, character):
+def check_for_challenge(board: list, character: dict) -> 'True':
+    """
+    Returns True if the character encounters a challenge on the board.
+
+    :param board: must be a list of rows and columns
+    :param character: must be a dictionary
+    :return: True if the character encounters a challenge on the board
+    """
     if board[(character["X-Coordinate"], character["Y-Coordinate"])] == "$  ":
         print()
         print("There is a puzzle here.")
